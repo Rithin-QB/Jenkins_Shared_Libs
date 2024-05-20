8666cbeab36fdec18359575b82574c50c72db80e
Rithin-QB <144108213+Rithin-QB@users.noreply.github.com>
2024-05-20 14:16:12 +0530

    Update buildDockerImage.groovy

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,3 +1,3 @@
 def call(String dockerfilePath, String imageName, String imageTag) {
-    sh "sudo docker build -t ${imageName}:${imageTag} -f ${dockerfilePath} ."
+    sh "docker build -t ${imageName}:${imageTag} -f ${dockerfilePath} ."
 }
