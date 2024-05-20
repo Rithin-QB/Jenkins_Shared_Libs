ba3621ded765e7120c92214f64a5008acc1f8911
Rithin-QB <144108213+Rithin-QB@users.noreply.github.com>
2024-05-20 23:50:50 +0530

    Update pushDockerImage.groovy

--- vars/pushDockerImage.groovy
+++ vars/pushDockerImage.groovy
@@ -1,4 +1,4 @@
 def call(String imageName, String imageTag, String dockerRepoUrl) {
     sh "docker tag ${imageName}:${imageTag} ${dockerRepoUrl}:${imageTag}"
-    sh "docker push ${dockerRepoUrl}/${imageName}:${imageTag}"
+    sh "docker push ${dockerRepoUrl}:${imageTag}"
 }
